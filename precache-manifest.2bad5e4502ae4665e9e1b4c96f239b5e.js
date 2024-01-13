d2cd9678ad86abbb608cc7af9cd6c54260920978
JoseCedrim <jose.cedrim@hotmail.com>
2024-01-12 21:31:08 -0300

    Updates

---
+++ precache-manifest.2bad5e4502ae4665e9e1b4c96f239b5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb22f2581f50e6f0ef5ba2e720ce86e",
+    "url": "/Clone-Netflix-2/index.html"
+  },
+  {
+    "revision": "8425989304bf8eb8f1cb",
+    "url": "/Clone-Netflix-2/static/css/main.6690849d.chunk.css"
+  },
+  {
+    "revision": "2018f9cb7f9bd672ff91",
+    "url": "/Clone-Netflix-2/static/js/2.aebc7e73.chunk.js"
+  },
+  {
+    "revision": "8425989304bf8eb8f1cb",
+    "url": "/Clone-Netflix-2/static/js/main.7119a792.chunk.js"
+  },
+  {
+    "revision": "850e4dd45c5277fa1151",
+    "url": "/Clone-Netflix-2/static/js/runtime~main.4af497d1.js"
+  },
+  {
+    "revision": "149a098f51438682ddb64f6e0c0c709b",
+    "url": "/Clone-Netflix-2/static/media/landingPage.149a098f.jpg"
+  },
+  {
+    "revision": "8562b6565f5ae1db5e4af40d85b4ed2d",
+    "url": "/Clone-Netflix-2/static/media/netflix.8562b656.png"
+  },
+  {
+    "revision": "991556c2526eb97f8e745b79363c8661",
+    "url": "/Clone-Netflix-2/static/media/normal.991556c2.jpg"
+  },
+  {
+    "revision": "c2b89779dd6b85078fc0cc2f3a9aaf28",
+    "url": "/Clone-Netflix-2/static/media/profile.c2b89779.jpg"
+  }
+]);
